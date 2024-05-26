da70f840ee7b02fa1b6f0dd2718f69cb89cead59
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2024-05-27 00:24:47 +0530

    Feat: Add expense component with updated progressbar

--- app/(routes)/dashboard/budget/_components/BudgetItem.jsx
+++ app/(routes)/dashboard/budget/_components/BudgetItem.jsx
@@ -1,8 +1,14 @@
+import { Link } from 'lucide-react'
 import React from 'react'
 
 const BudgetItem = ({ budget }) => {
+    const calculateProgressPercentage = () => {
+        const percentage = (budget.totalSpend / budget.amount) * 100
+        return percentage.toFixed(2)
+    }
+
     return (
-        <div className='p-5 border rounded-lg hover:shadow-md cursor-pointer' >
+        <Link href={'/dashboard/expenses/' + budget?.id} className='p-5 border rounded-lg hover:shadow-md cursor-pointer h-[170px]' >
             <div className='flex gap-2 items-center justify-between'>
                 <div className='flex gap-2 items-center'>
                     <h2 className='text-2xl p-3 bg-slate-100 rounded-full'>{budget?.icon}</h2>
@@ -19,12 +25,12 @@ const BudgetItem = ({ budget }) => {
                     <h2 className='text-xs text-slate-400'>₹{budget.amount - budget.totalSpend} Remaining</h2>
                 </div>
                 <div className='w-full bg-slate-300 h-2 rounded-full'>
-                    <div className='w-[40%] bg-primary h-2 rounded-full'>
+                    <div className='bg-primary h-2 rounded-full' style={{ width: `${calculateProgressPercentage()}%` }}>
 
                     </div>
                 </div>
             </div>
-        </div>
+        </Link>
     )
 }
 
