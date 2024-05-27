ec3000dc16b9064512cb0081a5ce36c23d0b4bf2
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2024-05-27 11:18:47 +0530

    Feat: Add profile icon on homepage

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -3,20 +3,19 @@ import React from 'react'
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
-// import { useUser, UserButton} from '@clerk/nextjs'
+import { useUser, UserButton} from '@clerk/nextjs'
 
 const Header = () => {
-
-    // const {user, isSignedIn} = useUser()
+    const {user, isSignedIn} = useUser()
     return (
         <div className='p-5 flex justify-between items-center border shadow-sm'>
             <Image src={'/logo.svg'} alt='logo' width={160} height={100} />
-            {/* {isSignedIn ?
+            {isSignedIn ?
                 <UserButton /> :
                 <Link href={'/dashboard'}>
                     <Button>Get Started</Button>
                 </Link>
-            } */}
+            }
         </div>
     )
 }
