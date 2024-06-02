4e8b739e689e5ad77f194a1d9d90ad8a07572053
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2024-06-02 22:01:53 +0530

    Feat: Add Expense Table

--- app/(routes)/dashboard/expenses/_components/ExpenseListTable.jsx
+++ app/(routes)/dashboard/expenses/_components/ExpenseListTable.jsx
@@ -23,7 +23,8 @@ const ExpenseListTable = ({ expenseList, refreshData }) => {
 
     return (
         <div className='mt-3'>
-            <div className='grid grid-cols-4 bg-slate-200 p-2 font-bold'>
+            <h2 className='font-bold text-lg'>Latest Expenses</h2>
+            <div className='grid grid-cols-4 bg-slate-200 p-2 font-bold mt-3'>
                 <h2>Name</h2>
                 <h2>Amount</h2>
                 <h2>Date</h2>
--- app/(routes)/dashboard/page.jsx
+++ app/(routes)/dashboard/page.jsx
@@ -6,10 +6,13 @@ import BarChartDashboard from './_components/BarChartDashboard'
 import { db } from '@/utils/dbConfig'
 import { desc, eq, getTableColumns, sql } from 'drizzle-orm'
 import { Budgets, Expenses } from '@/utils/schema'
+import BudgetItem from './budget/_components/BudgetItem'
+import ExpenseListTable from './expenses/_components/ExpenseListTable'
 
 const Dashboard = () => {
   const { user } = useUser()
   const [budgetList, setBudgetList] = useState([])
+  const [expensesList, setExpensesList] = useState([])
 
   useEffect(() => {
     user && getBudgetList()
@@ -25,6 +28,20 @@ const Dashboard = () => {
       .groupBy(Budgets.id)
       .orderBy(desc(Budgets.id))
     setBudgetList(result)
+    getAllExpenses()
+  }
+
+  const getAllExpenses = async () => {
+    const result = await db.select({
+      id: Expenses.id,
+      name: Expenses.name,
+      amount: Expenses.amount,
+      createdAt: Expenses.createdAt
+    }).from(Budgets)
+      .rightJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
+      .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAddress))
+      .orderBy(desc(Expenses.id))
+    setExpensesList(result)
   }
 
   return (
@@ -32,12 +49,16 @@ const Dashboard = () => {
       <h2 className='font-bold text-3xl'>Hi, {user?.fullName ?? 'Guest'} ✌️</h2>
       <p className='text-gray-500'>Here&apos;s what&apos;s happening with your money. Let&apos;s manage your expenses.</p>
       <CardInfo budgetList={budgetList} />
-      <div className='grid grdi-cols-1 md:grid-cols-3 mt-6'>
+      <div className='grid grdi-cols-1 md:grid-cols-3 mt-6 gap-5'>
         <div className='md:col-span-2'>
           <BarChartDashboard budgetList={budgetList} />
+          <ExpenseListTable expenseList={expensesList} refreshData={()=> getBudgetList()}/>
         </div>
-        <div>
-          Other
+        <div className='grid gap-1'>
+          <h2 className='font-bold text-lg'>Latest Budgets</h2>
+          {budgetList.map((budget, index) => (
+            <BudgetItem key={index} budget={budget} />
+          ))}
         </div>
       </div>
     </div>
